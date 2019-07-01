1f2d474d0728d782552a679960c386e9637f791f
alongd <alongd@mit.edu>
2019-07-01 13:55:54 -0400

    Tests: Converter test_set_rdkit_dihedrals

--- arc/species/converterTest.py
+++ arc/species/converterTest.py
@@ -8,6 +8,8 @@ This module contains unit tests of the arc.species.converter module
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 import unittest
 
+from rdkit.Chem import rdMolTransforms as rdMT
+
 from rmgpy.molecule.molecule import Molecule
 from rmgpy.species import Species
 
@@ -813,6 +815,125 @@ O       2.17315400   -0.03069900   -0.09349100"""
         self.assertEqual(len(mol3.atoms), 11)
         self.assertEqual(len(mol4.atoms), 24)
         self.assertEqual(len(mol5.atoms), 3)
+
+    def test_set_rdkit_dihedrals(self):
+        """Test setting the dihedral of an RDKit molecule"""
+        mol0 = Molecule().fromSMILES(str('CCO'))
+        xyz0 = """O       1.17961475   -0.92725986    0.15472373
+C       0.45858928    0.27919340   -0.04589251
+C      -1.02470597   -0.01894626    0.00226686
+H       0.73480842    0.69726202   -1.01850832
+H       0.73330833    0.98882191    0.74024781
+H      -1.29861662   -0.45953441    0.96660817
+H      -1.29713649   -0.74721756   -0.76877222
+H      -1.61116041    0.89155300   -0.14917209
+H       2.12529871   -0.70387223    0.11849858"""
+
+        torsion0 = (3, 2, 1, 9)  # the OH rotor
+        new_dihedral = -60
+        deg_increment = 240  # -180 + 240 = +60
+
+        conf, rd_mol, indx_map = converter.rdkit_conf_from_mol(mol0, xyz0)
+        rd_tor_map = [indx_map[i - 1] for i in torsion0]  # convert the atom indices in the torsion to RDKit indices
+        new_coord1 = converter.set_rdkit_dihedrals(conf, rd_mol, indx_map, rd_tor_map, deg_abs=new_dihedral)
+        new_xyz1 = converter.get_xyz_string(coord=new_coord1, mol=mol0)
+
+        conf, rd_mol, indx_map = converter.rdkit_conf_from_mol(mol0, xyz0)  # convert again to init the conf object
+        rd_tor_map = [indx_map[i - 1] for i in torsion0]  # convert the atom indices in the torsion to RDKit indices
+        new_coord2 = converter.set_rdkit_dihedrals(conf, rd_mol, indx_map, rd_tor_map, deg_increment=deg_increment)
+        new_xyz2 = converter.get_xyz_string(coord=new_coord2, mol=mol0)
+
+        expected_xyz1 = """O       1.17961475   -0.92725986    0.15472373
+C       0.45858928    0.27919340   -0.04589251
+C      -1.02470597   -0.01894626    0.00226686
+H       0.73480842    0.69726202   -1.01850832
+H       0.73330833    0.98882191    0.74024781
+H      -1.29861662   -0.45953441    0.96660817
+H      -1.29713649   -0.74721756   -0.76877222
+H      -1.61116041    0.89155300   -0.14917209
+H       0.92345327   -1.27098714    1.02751540
+"""
+        expected_xyz2 = """O       1.17961475   -0.92725986    0.15472373
+C       0.45858928    0.27919340   -0.04589251
+C      -1.02470597   -0.01894626    0.00226686
+H       0.73480842    0.69726202   -1.01850832
+H       0.73330833    0.98882191    0.74024781
+H      -1.29861662   -0.45953441    0.96660817
+H      -1.29713649   -0.74721756   -0.76877222
+H      -1.61116041    0.89155300   -0.14917209
+H       0.92480849   -1.53430645   -0.56088835
+"""
+
+        self.assertEqual(new_xyz1, expected_xyz1)
+        self.assertEqual(new_xyz2, expected_xyz2)
+
+        mol1 = Molecule().fromSMILES(str('CC(C)(C#N)/N=N/C(C)(C)C#N'))  # AIBN
+        xyz1 = """N      -0.29070308    0.26322835    0.48770927
+N       0.29070351   -0.26323281   -0.48771096
+N      -2.61741263    1.38275080    2.63428181
+N       2.61742270   -1.38276006   -2.63427425
+C      -1.77086206    0.18100754    0.43957605
+C       1.77086254   -0.18101028   -0.43957552
+C      -2.22486176   -1.28143567    0.45202312
+C      -2.30707039    0.92407663   -0.78734681
+C       2.30707074   -0.92407071    0.78735246
+C       2.22485929    1.28143406   -0.45203080
+C      -2.23868798    0.85547218    1.67084736
+C       2.23869247   -0.85548109   -1.67084185
+H      -1.90398693   -1.81060764   -0.45229645
+H      -3.31681639   -1.35858536    0.51240600
+H      -1.80714051   -1.81980551    1.31137107
+H      -3.40300863    0.95379538   -0.78701415
+H      -1.98806037    0.44494681   -1.71978670
+H      -1.94802915    1.96005927   -0.81269573
+H       1.98805486   -0.44493850    1.71978893
+H       1.94803425   -1.96005464    0.81270509
+H       3.40300902   -0.95378386    0.78702431
+H       1.90398036    1.81061002    0.45228426
+H       3.31681405    1.35858667   -0.51241516
+H       1.80713611    1.81979843   -1.31138136"""
+
+        torsion1 = (1, 2, 6, 9)
+        new_dihedral = 118.2
+
+        conf, rd_mol, indx_map = converter.rdkit_conf_from_mol(mol1, xyz1)
+        rd_tor_map = [indx_map[i - 1] for i in torsion1]  # convert the atom indices in the torsion to RDKit indices
+        new_coord3 = converter.set_rdkit_dihedrals(conf, rd_mol, indx_map, rd_tor_map, deg_abs=new_dihedral)
+        new_xyz3 = converter.get_xyz_string(coord=new_coord3, mol=mol1)
+
+        expected_xyz3 = """N      -0.29070308    0.26322835    0.48770927
+N       0.29070351   -0.26323281   -0.48771096
+N      -2.61741263    1.38275080    2.63428181
+N       2.48573367    1.01638899   -2.68295766
+C      -1.77086206    0.18100754    0.43957605
+C       1.77086254   -0.18101028   -0.43957552
+C      -2.22486176   -1.28143567    0.45202312
+C      -2.30707039    0.92407663   -0.78734681
+C       2.38216062   -1.58430507   -0.39387342
+C       2.21983062    0.66527087    0.75509913
+C      -2.23868798    0.85547218    1.67084736
+C       2.16482620    0.49023713   -1.69815092
+H      -1.90398693   -1.81060764   -0.45229645
+H      -3.31681639   -1.35858536    0.51240600
+H      -1.80714051   -1.81980551    1.31137107
+H      -3.40300863    0.95379538   -0.78701415
+H      -1.98806037    0.44494681   -1.71978670
+H      -1.94802915    1.96005927   -0.81269573
+H       2.11909310   -2.10839740    0.53181512
+H       2.02775663   -2.19945525   -1.22981644
+H       3.47613291   -1.54390687   -0.45350823
+H       1.95308217    0.19222185    1.70685860
+H       3.30593713    0.81467275    0.75113509
+H       1.74954927    1.65592664    0.73932447
+"""
+
+        self.assertEqual(new_xyz3, expected_xyz3)
+
+        rd_conf, rd_mol, indx_map = converter.rdkit_conf_from_mol(mol1, expected_xyz3)
+        rd_scan = [indx_map[i - 1] for i in torsion1]  # convert the atom indices to RDKit indices
+        angle = rdMT.GetDihedralDeg(rd_conf, rd_scan[0], rd_scan[1], rd_scan[2], rd_scan[3])
+
+        self.assertAlmostEqual(angle, 118.2, 5)
 
     def set_radicals_correctly_from_xyz(self):
         """Test that we determine the number of radicals correctly from given xyz and multiplicity"""
