02a633d0a3ace9b92676d493e55e5e073e7a6bd8
alongd <alongd@mit.edu>
2019-07-01 16:34:11 -0400

    Minor: Comment correction in settings.py

--- arc/settings.py
+++ arc/settings.py
@@ -76,7 +76,7 @@ default_job_types = {'conformers': True,      # defaults to True if not specifie
                      }
 
 # List here (complete or partial) phrases of methods or basis sets you'd like to associate to specific ESS
-# Avoid ascribing the same phrase to more than one server, this may cause undeterministic assignment of software
+# Avoid ascribing the same phrase to more than one software, this may cause undeterministic assignment of software
 # Format is levels_ess = {ess: ['phrase1', 'phrase2'], ess2: ['phrase3', 'phrase3']}
 levels_ess = {
     'gaussian': ['b3lyp', 'm062x'],
