a41b06a3974863a34e0c0e94dacfaf2eeb3aff06
alongd <alongd@mit.edu>
2019-07-02 08:53:57 -0400

    common

--- arc/common.py
+++ arc/common.py
@@ -17,6 +17,8 @@ import shutil
 import subprocess
 import yaml
 
+from arkane.common import get_element_mass
+
 from arc.settings import arc_path, servers
 from arc.arc_exceptions import InputError, SettingsError
 
@@ -25,23 +27,6 @@ from arc.arc_exceptions import InputError, SettingsError
 logger = logging.getLogger('arc')
 
 VERSION = '1.0.0'
-
-
-def read_file(path):
-    """
-    Read the ARC YAML input file and return the parameters in a dictionary.
-
-    Args:
-        (str, unicode): The input file path.
-
-    Returns:
-        dict: The input dictionary read from the file.
-    """
-    if not os.path.isfile(path):
-        raise InputError('Could not find the input file {0}'.format(path))
-    with open(path, 'r') as f:
-        input_dict = yaml.load(stream=f, Loader=yaml.FullLoader)
-    return input_dict
 
 
 def time_lapse(t0):
@@ -94,7 +79,7 @@ def check_ess_settings(ess_settings=None):
                                 'strings. Got: {0} which is a {1}'.format(server_list, type(server_list)))
     # run checks:
     for ess, server_list in settings.items():
-        if ess.lower() not in ['gaussian', 'qchem', 'molpro', 'onedmin', 'orca']:
+        if ess.lower() not in ['gaussian', 'qchem', 'molpro', 'orca', 'terachem', 'onedmin', 'gromacs']:
             raise SettingsError('Recognized ESS software are Gaussian, QChem, Molpro, Orca or OneDMin. '
                                 'Got: {0}'.format(ess))
         for server in server_list:
@@ -254,14 +239,36 @@ def log_footer(execution_time, level=logging.INFO):
     logger.log(level, 'ARC execution terminated on {0}'.format(time.asctime()))
 
 
-def save_dict_file(path, restart_dict):
+def read_yaml_file(path):
     """
-    Save an input / restart YAML file
+    Read a YAML file (usually an input / restart file, but also conformers file)
+    and return the parameters as python variables.
+
+    Args:
+        path (str, unicode): The YAML file path to read.
+
+    Returns:
+        dict, list: The content read from the file.
+    """
+    if not os.path.isfile(path):
+        raise InputError('Could not find the YAML file {0}'.format(path))
+    with open(path, 'r') as f:
+        content = yaml.load(stream=f, Loader=yaml.FullLoader)
+    return content
+
+
+def save_yaml_file(path, content):
+    """
+    Save a YAML file (usually an input / restart file, but also conformers file)
+
+    Args:
+        path (str, unicode): The YAML file path to save.
+        content (str, unicode): The content to save.
     """
     yaml.add_representer(str, string_representer)
     yaml.add_representer(unicode, unicode_representer)
     logger.debug('Creating a restart file...')
-    content = yaml.dump(data=restart_dict, encoding='utf-8', allow_unicode=True)
+    content = yaml.dump(data=content, encoding='utf-8', allow_unicode=True)
     with open(path, 'w') as f:
         f.write(content)
 
@@ -278,3 +285,89 @@ def unicode_representer(dumper, data):
     if len(data.splitlines()) > 1:
         return yaml.ScalarNode(tag='tag:yaml.org,2002:str', value=data, style='|')
     return yaml.ScalarNode(tag='tag:yaml.org,2002:str', value=data)
+
+
+def get_ordinal_indicator(number):
+    """
+    Returns the ordinal indicator for an integer.
+
+    Args:
+        number (int): An integer for which the ordinal indicator will be determined.
+
+    Returns:
+        str, unicode: The integer's ordinal indicator.
+    """
+    ordinal_dict = {1: 'st', 2: 'nd', 3: 'rd'}
+    if number > 13 :
+        number %= 10
+    if number in ordinal_dict.keys():
+        return ordinal_dict[number]
+    return 'th'
+
+
+# ATOM_RADII data taken from DOI: 10.1039/b801115j
+ATOM_RADII = {'H': 0.31, 'He': 0.28,
+              'Li': 1.28, 'Be': 0.96, 'B': 0.84, 'C': 0.76, 'N': 0.71, 'O': 0.66, 'F': 0.57, 'Ne': 0.58,
+              'Na': 1.66, 'Mg': 1.41, 'Al': 1.21, 'Si': 1.11, 'P': 1.07, 'S': 1.05, 'Cl': 1.02, 'Ar': 1.06,
+              'K': 2.03, 'Ca': 1.76, 'Sc': 1.70, 'Ti': 1.60, 'V': 1.53, 'Cr': 1.39, 'Mn': 1.39, 'Fe': 1.32,
+              'Co': 1.26, 'Ni': 1.24, 'Cu': 1.32, 'Zn': 1.22, 'Ga': 1.22, 'Ge': 1.20, 'As': 1.19,
+              'Se': 1.20, 'Br': 1.20, 'Kr': 1.16, 'Rb': 2.20, 'Sr': 1.95, 'Y': 1.90, 'Zr': 1.75,
+              'Nb': 1.64, 'Mo': 1.54, 'Tc': 1.47, 'Ru': 1.46, 'Rh': 1.42, 'Pd': 1.39, 'Ag': 1.45,
+              'Cd': 1.44, 'In': 1.42, 'Sn': 1.39, 'Sb': 1.39, 'Te': 1.38, 'I': 1.39, 'Xe': 1.40,
+              'Cs': 2.44, 'Ba': 2.15, 'Pt': 1.36, 'Au': 1.36, 'Hg': 1.32, 'Tl': 1.45, 'Pb': 1.46,
+              'Bi': 1.48, 'Po': 1.40, 'At': 1.50, 'Rn': 1.50, 'Fr': 2.60, 'Ra': 2.21, 'U': 1.96}
+
+
+def get_atom_radius(symbol):
+    """
+    Get the atom covalent radius in Angstroms, data in the ATOM_RADII dict.
+    (Change to QCElemental after transitioning to Py3)
+
+    Args:
+        symbol (str, unicode): The atomic symbol.
+
+    Returns:
+        float: The atomic covalent radius (None if not found).
+    """
+    if not isinstance(symbol, (str, unicode)):
+        raise InputError('the symbol argument must be string, got {0} which is a {1}'.format(symbol, type(symbol)))
+
+    if symbol in ATOM_RADII:
+        return ATOM_RADII[symbol]
+    else:
+        return None
+
+
+def get_center_of_mass(xyz=None, coords=None, symbols=None):
+    """
+    Get the center of mass of xyz coordinates.
+    Assumes arc.converter.standardize_xyz_string() was already called for xyz.
+    Note that xyz from ESS output is usually already centered at the center of mass (to some precision).
+
+    Args:
+        xyz (list, string, unicode): The xyz coordinates in a string.
+
+    Returns:
+        tuple: The center of mass coordinates.
+    """
+    if xyz is not None:
+        masses, coords = list(), list()
+        for line in xyz.splitlines():
+            if line.strip():
+                splits = line.split()
+                masses.append(get_element_mass(str(splits[0]))[0])
+                coords.append([float(splits[1]), float(splits[2]), float(splits[3])])
+    elif coords is not None and symbols is not None:
+        masses = [get_element_mass(str(symbol))[0] for symbol in symbols]
+    else:
+        raise InputError('Either xyz or coords and symbols must be given')
+    cm_x, cm_y, cm_z = 0, 0, 0
+    for coord, mass in zip(coords, masses):
+        cm_x += coord[0] * mass
+        cm_y += coord[1] * mass
+        cm_z += coord[2] * mass
+    cm_x /= sum(masses)
+    cm_y /= sum(masses)
+    cm_z /= sum(masses)
+    return cm_x, cm_y, cm_z
+
