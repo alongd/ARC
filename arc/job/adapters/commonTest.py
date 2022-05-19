c4aa457a7c209645f03e2c215e9b3110fc632570
Alon Grinberg Dana <alon@technion.ac.il>
2022-05-19 09:51:30 +0300

    f! adapters common test

--- arc/job/adapters/commonTest.py
+++ arc/job/adapters/commonTest.py
@@ -6,6 +6,7 @@ This module contains unit tests of the arc.job.adapters.common module
 """
 
 import os
+import shutil
 import unittest
 
 import arc.job.adapters.common as common
@@ -37,6 +38,7 @@ class TestJobCommon(unittest.TestCase):
                                     )
         cls.job_2 = GaussianAdapter(execution_type='incore',
                                     job_type='scan',
+                                    torsions=[[1, 2, 3, 4]],
                                     level=Level(method='wb97xd', basis='def2tzvp'),
                                     project='test',
                                     project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_GaussianAdapter'),
@@ -46,6 +48,7 @@ class TestJobCommon(unittest.TestCase):
                                     )
         cls.job_3 = GaussianAdapter(execution_type='incore',
                                     job_type='scan',
+                                    torsions=[[1, 2, 3, 4]],
                                     level=Level(method='wb97xd', basis='def2tzvp'),
                                     project='test',
                                     project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_GaussianAdapter'),
@@ -53,31 +56,6 @@ class TestJobCommon(unittest.TestCase):
                                     testing=True,
                                     args={'keyword': {'general': 'IOp(1/12=5,3/44=0)'}},
                                     )
-        cls.job_4 = MolproAdapter(execution_type='incore',
-                                  job_type='irc',
-                                  level=Level(method='ccsd(t)', basis='cc-pvtz'),
-                                  project='test',
-                                  project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_MolproAdapter'),
-                                  species=[ARCSpecies(label='spc1', xyz=['O 0 0 1'], multiplicity=1)],
-                                  testing=True,
-                                  )
-        cls.job_5 = GaussianAdapter(execution_type='incore',
-                                    job_type='irc',
-                                    level=Level(method='b3lyp', basis='def2svp'),
-                                    project='test',
-                                    project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_GaussianAdapter'),
-                                    species=[ARCSpecies(label='spc1', xyz=['O 0 0 1'], multiplicity=1)],
-                                    testing=True,
-                                    args={'keyword': {'general': 'IOp(1/12=5,3/44=0)'}},
-                                    )
-        cls.job_6 = MolproAdapter(execution_type='incore',
-                                  job_type='scan',
-                                  level=Level(method='ccsd(t)', basis='cc-pvtz'),
-                                  project='test',
-                                  project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_MolproAdapter'),
-                                  species=[ARCSpecies(label='ethane', smiles='CC')],
-                                  testing=True,
-                                  )
 
     def test_is_restricted(self):
         """Test the is_restricted() function"""
@@ -90,34 +68,61 @@ class TestJobCommon(unittest.TestCase):
         common.check_argument_consistency(self.job_1)
         common.check_argument_consistency(self.job_2)
         with self.assertRaises(NotImplementedError):
-            common.check_argument_consistency(self.job_4)
+            MolproAdapter(execution_type='incore',
+                          job_type='irc',
+                          level=Level(method='ccsd(t)', basis='cc-pvtz'),
+                          project='test',
+                          project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_MolproAdapter'),
+                          species=[ARCSpecies(label='spc1', xyz=['O 0 0 1'], multiplicity=1)],
+                          testing=True,
+                          )
         with self.assertRaises(ValueError):
-            common.check_argument_consistency(self.job_5)
-        self.job_6.species[0].determine_rotors()
+            GaussianAdapter(execution_type='incore',
+                            job_type='irc',
+                            level=Level(method='b3lyp', basis='def2svp'),
+                            project='test',
+                            project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_GaussianAdapter'),
+                            species=[ARCSpecies(label='spc1', xyz=['O 0 0 1'], multiplicity=1)],
+                            testing=True,
+                            args={'keyword': {'general': 'IOp(1/12=5,3/44=0)'}},
+                            )
         with self.assertRaises(NotImplementedError):
-            common.check_argument_consistency(self.job_6)
+            spc = ARCSpecies(label='ethane', smiles='CC')
+            spc.determine_rotors()
+            spc.rotors_dict['directed_scan_type'] = 'ess'
+            MolproAdapter(execution_type='incore',
+                          job_type='scan',
+                          torsions=[[1, 2, 3, 4]],
+                          level=Level(method='ccsd(t)', basis='cc-pvtz'),
+                          project='test',
+                          project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_MolproAdapter'),
+                          species=[spc],
+                          testing=True,
+                          )
         self.job_2.scan_res = 55.6
         with self.assertRaises(ValueError):
             common.check_argument_consistency(self.job_2)
-        with self.assertRaises(ValueError):
-            common.check_argument_consistency(self.job_3)
 
     def test_update_input_dict_with_args(self):
         """Test the update_input_dict_with_args() function"""
         input_dict = common.update_input_dict_with_args(args={}, input_dict={})
         self.assertEqual(input_dict, dict())
 
-        input_dict = common.update_input_dict_with_args(args={'block': """a\nb"""}, input_dict={})
-        self.assertEqual(input_dict, {'block': """\n\na\nb"""})
+        input_dict = common.update_input_dict_with_args(args={'block': {'1': """a\nb"""}},
+                                                        input_dict={'block': ''})
+        self.assertEqual(input_dict, {'block': """\n\na\nb\n"""})
 
-        input_dict = common.update_input_dict_with_args(args={'block': """a\nb"""}, input_dict={'block': """x\ny\n"""})
+        input_dict = common.update_input_dict_with_args(args={'block': {'1': """a\nb"""}},
+                                                        input_dict={'block': """x\ny\n"""})
         self.assertEqual(input_dict, {'block': """x\ny\na\nb\n"""})
 
-        input_dict = common.update_input_dict_with_args(args={'keyword': {'scan_trsh': 'keyword 1'}}, input_dict={})
-        self.assertEqual(input_dict, {'scan_trsh': 'keyword 1'})
+        input_dict = common.update_input_dict_with_args(args={'keyword': {'scan_trsh': 'keyword 1'}},
+                                                        input_dict={})
+        self.assertEqual(input_dict, {'scan_trsh': 'keyword 1 '})
 
-        input_dict = common.update_input_dict_with_args(args={'keyword': {'opt': 'keyword 2'}}, input_dict={})
-        self.assertEqual(input_dict, {'keywords': 'keyword 2'})
+        input_dict = common.update_input_dict_with_args(args={'keyword': {'opt': 'keyword 2'}},
+                                                        input_dict={})
+        self.assertEqual(input_dict, {'keywords': 'keyword 2 '})
 
     def test_set_job_args(self):
         """Test the set_job_args() function"""
@@ -146,6 +151,15 @@ class TestJobCommon(unittest.TestCase):
 
         ans = common.which(command=['python'], return_bool=False, raise_error=False)
         self.assertIn('bin/python', ans)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        A function that is run ONCE after all unit tests in this class.
+        Delete all project directories created during these unit tests
+        """
+        shutil.rmtree(os.path.join(ARC_PATH, 'arc', 'testing', 'test_GaussianAdapter'), ignore_errors=True)
+        shutil.rmtree(os.path.join(ARC_PATH, 'arc', 'testing', 'test_MolproAdapter'), ignore_errors=True)
 
 
 if __name__ == '__main__':
